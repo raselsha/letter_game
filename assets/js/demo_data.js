bbc82d8563b67f45d4d8498950252c146d373f3e
Shahahdat <raselsha@gmail.com>
2021-07-12 14:32:15 +0600

    added some different sound

--- assets/js/demo_data.js
+++ assets/js/demo_data.js
@@ -2,10 +2,10 @@ var tableA=['a1','a2'];
 var tableB=['b1','b2','b3'];
 var joinLetters=['j1','j2','j3','j4','j5','j6'];
 var words={
-			text:['bake','band','bar','word 4','word 5','word 6',],
+			text:['bake','band','bar','bark','word 5','word 6',],
 			sound:['w1','w2','w3','w4','w5','w6'],
 		};
 var sentences={
-			text:['I want to <b></b> cup cakes','he works in a music or <b></b>','He works in a <b></b>','sentence <b></b> 5','<b></b> sentence 5','sentence 6 <b></b>',],
+			text:['I want to <b></b> cup cakes','he works in a music or <b></b>','you become a <b></b>','My dog does\'t <b></b> at night','<b></b> sentence 5','sentence 6 <b></b>',],
 			sound:['s1','s2','s3','s4','s5','s6']
 		};
